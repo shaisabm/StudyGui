c076f3992e92c178b424083084d5fc5384f012a7
Shaisab Mistry <shaisabmistry@gmail.com>
2024-06-07 19:39:59 -0400

    Cleaned up the profile fields

--- base/forms.py
+++ base/forms.py
@@ -1,7 +1,9 @@
 from django.forms import ModelForm
 from .models import Room, Profile
 from django.contrib.auth.models import User
-
+from django.forms.widgets import ClearableFileInput
+from django.utils.html import escape, conditional_escape
+from django.utils.translation import gettext_lazy as _
 
 class RoomForm(ModelForm):
     class Meta:
@@ -15,9 +17,24 @@ class UserForm(ModelForm):
         model = User
         fields = ['username','email']
 
+
+
+class CustomClearableFileInput(ClearableFileInput):
+    template_name = 'django/forms/widgets/clearable_file_input.html'
+    intial_text = ('Currently')
+    input_text = ('Change')
+
+    def get_context(self, name, value, attrs):
+        context = super().get_context(name, value, attrs)
+        context['widget']['is_initial'] = False
+        return context
+
 class UserProfile(ModelForm):
     class Meta:
         model = Profile
         fields = ['profile_pic','bio']
         exclude = ['user']
+        widgets = {
+            'profile_pic' : CustomClearableFileInput
+        }
 
